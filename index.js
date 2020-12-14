2cc67c4371a3a157be60cce60f5085327a52d101
tbanish <tombanish@gmail.com>
2020-12-14 16:48:33 -0500

    Done.

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ BoardMember.prototype.doCharity = function () {return "I like to help people."}
 
 BoardMember.prototype.releasePressStatement = function () {return "You will see great things from Scuber."}
 
-BoardMember.prototype.sayHi = function () {return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.``}
+BoardMember.prototype.sayHi = function () {return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
